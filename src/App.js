72a28e1eefa798b4f4e7a1d4d1a15c39afea461d
nicholas <nicholas@usp.br>
2023-11-22 12:44:03 -0300

    Changes browser router to hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './Components/HomePage';  // Import the HomePage component
 import Header from './Components/Header/header';
 import PartsContext from './Contexts/PartsContext';
