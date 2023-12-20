2f860d07e39959e92f6c1f1a94e38ad4e770575d
nicholas <nicholas@usp.br>
2023-12-20 01:56:40 -0300

    patch 1.05 update

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="bg-gray-800 text-white w-full fixed top-0 left-0 z-10 shadow-md">
       <Container> {/* Use the Container component */}
-        <nav className="py-3 flex justify-start items-center h-full">
+        <nav className="py-3 flex justify-between items-center h-full">
           <ul className="flex items-center">
             <li className="mr-6">
               <Link to="/" className="text-white hover:text-gray-300">Home</Link>
@@ -18,6 +18,9 @@ function Header() {
               <Link to="/HowToUse" className="text-white hover:text-gray-300">How to use</Link>
             </li>
           </ul>
+          <div className="text-yellow-500 font-semibold text-normal sm:text-sm">
+            Updated with patch 1.05 changes!
+          </div>
         </nav>
       </Container>
     </header>
